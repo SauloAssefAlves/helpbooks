2c853cef36b99720f1a9d26e8ef59ae80835354a
SauloAssefAlves <saulo.assef98@gmail.com>
2022-11-22 11:39:23 -0300

    styled components instalado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 import Header from "./componentes/Header";
 
-
 function App() {
   return (
-    <div className="App">  
-    <Header/>
+    <div className="App">
+      <Header />
     </div>
   );
 }
